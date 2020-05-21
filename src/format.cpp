39101c5221bb51c8db54e40386f41838489f8bf7
knaaga90 <knaaga90@gmail.com>
2020-05-21 01:07:55 -0400

    Helper function to format secs to hh:mm:ss

--- src/format.cpp
+++ src/format.cpp
@@ -1,5 +1,6 @@
 #include <string>
-
+#include <sstream>
+#include <iomanip>
 #include "format.h"
 
 using std::string;
@@ -8,4 +9,21 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+std::string num_format(const int number) {
+    std::stringstream ss;
+    ss << std::setw(2) << std::setfill('0') << number;
+    return ss.str();
+}
+
+string Format::ElapsedTime(long seconds) 
+{ 
+    int hh, mm, ss;
+
+    std::ostringstream timestream;
+    hh = seconds / 3600;
+    mm = (seconds % 3600) / 60;
+    ss = (seconds % 3600) % 60;
+    timestream  << num_format(hh) << ':' << num_format(mm) << ':' << num_format(ss);
+    return timestream.str(); 
+}
